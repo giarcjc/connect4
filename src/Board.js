401cd1201d3d64585a54adf8edc1e00eb5d16203
chriscraig <chris@cjcraig.com>
2018-06-16 15:42:43 -0600

    more board styling

--- src/Board.js
+++ src/Board.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import './Board.css';
 
+const bgColors = {
+  normal: 'white',
+  black: 'black',
+  red: 'red',
+};
+
 class Board extends Component {
   state = {};
   cellIds = Array.from({ length: 42 }, (_, i) => i);
-
+  status = () => {
+    return 'normal';
+  }
   render() {
 
     return (
-     <div className="game">
+     <div className="board">
        <div className="wrapper">
 
          {this.cellIds.map(id => {
 
            return (
              <div className="cell"
+             style={{backgroundColor: bgColors[this.status()] }}
              >{id}</div>
            )
          })}
